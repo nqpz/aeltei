abbf6c1738e915ba439773b6641489699cc5cbbc
Niels G. W. Serup <ngws@metanohi.name>
2018-10-17 20:52:21 +0200

    This seems to be necessary.

--- examples/calibrate-with-string.sh
+++ examples/calibrate-with-string.sh
@@ -3,4 +3,4 @@
 # Just pipe your characters to this script. The characters should be separated
 # by the pipe character.
 
-tr '|' '\t' | sed 'a\\t' | tr '\n' '\t' | aeltei --calibrate
+tr '|' '\t' | sed 'a\\t' | tr '\n' '\t' | ./aeltei-run --calibrate
